4129870704787ea367b00303fd4f3081764868c4
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-13 14:26:40 +0300

    added the routes to task managemnet

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+//task managemnt routes
+router.get("/dashboard-data", protect, getDashboardData);
+router.get("user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks); //Get all tasks (admin: all, User: assigned)
+router.get("/:id", protect, getTaskById); //get task by ID
+router.post("/", protect, adminOnly, createTask); //create a task (admin only)
+router.put("/:id", protect, updateTask); //create task details
+router.delete("/:id", protect, adminOnly, deleteTask); //delete a task (admin only)
+router.put("/:id/status", protect, updateTaskStatus); //update task status
+router.put("/:id/todo", protect, updateTaskChecklist); //update task checklist
+
+module.exports = router;
