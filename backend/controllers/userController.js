6586afe9671590c7b2daeee125900e9ab347f84c
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-13 14:15:25 +0300

    added usercontroller.js work

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,8 +10,47 @@ const bcrypt = require("bcryptjs");
 
 const getUsers = async (req, res) => {
     try {
+        const users = await User.find({ role: "member" }).select("-password");
+
+            //add task counts to each
+            const usersWithtaskCounts = await Promise.all(users.map(async (user) => {
+                const pendingTasks = await Task.countDocuments({ assignedTo: user._id, status: "Pending" });
+                const inProgressTasks = await Task.countDocuments({ assignedTo: user._id, status: "In Progress" });
+                const completedTasks = await Task.countDocuments({ assignedTo: user._id, status: "Completed" });
+
+                return {
+                    ...user._doc, //include all the existing user data
+                    pendingTasks,
+                    inProgressTasks,
+                    completedTasks,
+                };
+            }));
+
+            res.status(200).json(usersWithtaskCounts);
 
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 };
+
+//@desc Get User by ID
+//@route GET /api/users/:id
+//@access Private
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select("-password");
+        if (!user) return res.status(404).json({ message: "User not found" });
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+//@desc Delete a user (admin only)
+//@route DELETE /api/users/:id
+//@access Private (admin)
+const deleteUser = async (req, res) => {
+
+};
+
+module.exports = { getUsers, getUserById, deleteUser };
